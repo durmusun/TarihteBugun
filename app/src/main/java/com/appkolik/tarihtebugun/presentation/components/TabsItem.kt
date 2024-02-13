83f597ea4c21e1f1b3b00c4baafa1949839a2d68
bycoder4bem <durmus.un@4bem.com>
2024-02-13 22:56:58 +0300

    Bazı çözünürlüklerde tab text kayma sorunu çözüldü

--- app/src/main/java/com/appkolik/tarihtebugun/presentation/components/TabsItem.kt
+++ app/src/main/java/com/appkolik/tarihtebugun/presentation/components/TabsItem.kt
@@ -1,11 +1,13 @@
 package com.appkolik.tarihtebugun.presentation.components
 
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.appkolik.tarihtebugun.presentation.ui.theme.BackgroundDark
 import com.appkolik.tarihtebugun.presentation.ui.theme.Black
 import com.appkolik.tarihtebugun.presentation.ui.theme.Red800
 import com.appkolik.tarihtebugun.presentation.ui.theme.White
@@ -14,7 +16,7 @@ import com.appkolik.tarihtebugun.presentation.ui.theme.tabLabel
 @Composable
 fun TabsItem(label: String, clickable : () -> Unit) {
     Button(
-        modifier = Modifier.size(width = 96.dp, height = 40.dp),
+        modifier = Modifier.height(40.dp),
         onClick = clickable,
         colors = ButtonDefaults.buttonColors(
             contentColor = Black,
