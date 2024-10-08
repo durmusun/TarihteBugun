5d23e4a294b6ac36ec1031586bc1be21e1236e3d
durmusun <durmusun@gmail.com>
2024-10-08 23:01:12 +0300

    test

--- app/src/main/java/com/appkolik/tarihtebugun/presentation/components/Header.kt
+++ app/src/main/java/com/appkolik/tarihtebugun/presentation/components/Header.kt
@@ -26,6 +26,13 @@ fun Header() {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            modifier = Modifier.width(270.dp),
+            imageVector = ImageVector.vectorResource(
+                id = R.drawable.ic_people_1
+            ),
+            contentDescription = "man_01",
+        )
         Card(
             modifier = Modifier.offset(y = -5.dp),
             cardTitle = date.toString(),
